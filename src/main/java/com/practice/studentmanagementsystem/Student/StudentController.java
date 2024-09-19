268024a1d680fc53e4b395359d2a9211da636bab
DESKTOP-14DQ9CJ\msi <kyawzawwlinn.dev@gmail.com>
2024-09-19 22:10:11 +0630

    first commit

---
+++ src/main/java/com/practice/studentmanagementsystem/Student/StudentController.java
@@ -0,0 +1,29 @@
+package com.practice.studentmanagementsystem.Student;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("API/V1")
+public class StudentController {
+    private final StudentService studentService;
+
+    @Autowired
+    public StudentController(StudentService studentService) {
+        this.studentService = studentService;
+    }
+
+    @GetMapping
+    public List<Student> getAllStudents() {
+        return studentService.getAllStudents();
+    }
+
+
+
+
+}
